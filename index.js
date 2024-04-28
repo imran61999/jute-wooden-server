22f20756c8347ad3e28dc0ff4ed8ac3bc2df3796
imran61999 <smemrannit@gmail.com>
2024-04-28 16:03:21 +0600

    user post api ready

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
         res.send(users);
     })
 
+    app.post('/user', async(req, res)=>{
+        const user = req.body;
+        console.log(user);
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
