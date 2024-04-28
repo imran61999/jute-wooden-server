4018a26d7ab9ac15bcdf9e3b27f5b2731405ec73
imran61999 <smemrannit@gmail.com>
2024-04-28 14:56:08 +0600

    user get api ready

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 const app = express()
+
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
+console.log(process.env.DB_PASS)
+
 // emran
 // wMg7Ij3aprLkn4v0
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://emran:wMg7Ij3aprLkn4v0@cluster0.niwwhqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// const uri = "mongodb+srv://<username>:<password>@cluster0.niwwhqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.niwwhqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +30,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('juteDB').collection('user');
+
+
+    // user related api
+    app.get('/user', async(req, res) =>{
+        const cursor = userCollection.find();
+        const users = await cursor.toArray();
+        res.send(users);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
